a7db3e8c34e4f0226e73804c91ec39193ca7ed31
2018lopez <2018119251@ub.edu.bz>
2022-04-01 23:37:54 -0600

    Add Files

--- mms/server/server.js
+++ mms/server/server.js
@@ -1,11 +1,13 @@
 const express = require('express'),
     PORT = 3004,
+    cors = require('cors')
     bodyParser = require('body-parser'),
     server = express(),
     router = require('./router/router')
 
     server.use(bodyParser.json())
     .use(router)
+    .use(cors)
     .use(bodyParser.urlencoded({extended : true}))
     .listen(PORT,()=>console.log(
             `[+] server started on port ${PORT} 
